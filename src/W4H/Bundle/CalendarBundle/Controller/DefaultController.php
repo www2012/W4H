df25678309bc774eff596588e563c5b71e093514
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-02-09 14:24:15 +0100

    Rework header design, define Calendar controller actions

--- src/W4H/Bundle/CalendarBundle/Controller/DefaultController.php
+++ src/W4H/Bundle/CalendarBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace W4H\Bundle\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -10,10 +11,48 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="calendar")
-     * @Template()
+     * @Template("W4HCalendarBundle:Default:calendar.html.twig")
      */
     public function indexAction()
     {
+        //TODO:$tasks = getAll;
+        //TODO:return array('tasks' => $this->getIndexedTasks($tasks));
         return array();
+    }
+
+    /**
+     * @Route("/filter", name="calendar_filter")
+     * @Method("post")
+     * @Template("W4HCalendarBundle:Default:calendar.html.twig")
+     */
+    public function filterAction()
+    {
+        $request = $this->getRequest();
+        var_dump($request); die;
+        $form    = $this->createForm(new CalendarFilterType());
+        $form->bindRequest($request);
+        if ($form->isValid())
+        {
+            $data  = $form->getData();
+            $tasks = array();
+
+            // TODO : request all tasks matching filter
+            // $tasks = 
+            //TODO:return array('tasks' => $this->getIndexedTasks($tasks));
+            return array();
+        }
+
+        //TODO:$tasks = getAll;
+        //TODO:return array('tasks' => $this->getIndexedTasks($tasks));
+        return array();
+    }
+
+    /**
+     * @param DoctrineCollection Task
+     * @return array task indexed by location and schedule
+     */
+    private function getIndexedTasks($tasks)
+    {
+      return array();
     }
 }
