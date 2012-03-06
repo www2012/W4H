45891cd217c8d999f4a7eb2489e4b197ca2004ed
Pierre FERROLLIET <pierre.ferrolliet@idci-consulting.fr>
2012-03-06 17:33:08 +0100

    Load my event listener

--- src/W4H/Bundle/UserBundle/W4HUserBundle.php
+++ src/W4H/Bundle/UserBundle/W4HUserBundle.php
@@ -3,12 +3,22 @@
 namespace W4H\Bundle\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use W4H\Bundle\UserBundle\Listener\SendEmailListener;
+use Doctrine\ORM\Events;
 
 class W4HUserBundle extends Bundle
 {
     public function getParent()
     {
-        return 'SonataUserBundle';
+        return 'FOSUserBundle';
+    }
+
+    public function boot()
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $evm = $em->getEventManager();
+
+        $evm->addEventListener(array(Events::prePersist), new SendEmailListener($this->container));
     }
 }
 
