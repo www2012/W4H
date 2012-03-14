9daa989f120dc6f894e2c2d380eaaa12e88aa93f
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-03-14 07:48:33 +0100

    Add user bundle controller

---
+++ src/W4H/Bundle/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace W4H\Bundle\CalendarBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Pierre FERROLLIET <pierre.ferrolliet@idci-consulting.fr>
+ * @licence: GPL
+ *
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/data", name="user_data")
+     */
+    public function getDataAction(Request $request)
+    {
+        $format = $request->getRequestFormat();
+
+        return $this->render('W4HUserBundle:Default:data.'.$format.'.twig');
+    }
+}
+
