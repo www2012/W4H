469337671325c4492c78fe3f9f6ba48d3f250b3f
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-03-18 19:34:21 +0100

    Add user ws for jabber (return json|xml data for a given username/password) Fix fos_user_bundle views

--- src/W4H/Bundle/UserBundle/Controller/DefaultController.php
+++ src/W4H/Bundle/UserBundle/Controller/DefaultController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace W4H\Bundle\CalendarBundle\Controller;
+namespace W4H\Bundle\UserBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,13 +19,59 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/data", name="user_data")
+     * @Route("/admin/user/data.{_format}", name="user_data")
      */
     public function getDataAction(Request $request)
     {
-        $format = $request->getRequestFormat();
+        $format   = $request->getRequestFormat();
+        $username = $request->query->get('username'); //$request->request->get
+        $password = $request->query->get('password'); //$request->request->get
 
-        return $this->render('W4HUserBundle:Default:data.'.$format.'.twig');
+        $userManager = $this->get('fos_user.user_manager');
+        $person = $userManager->findUserByUsername($username);
+        $passwordValidator = $this->get('fos_user.validator.password');
+        $person->setPlainPassword($password);
+
+        $data = array(
+            'authentified' => 'true',
+            'account'      => array(
+                'firstname' => $person->getFirstName(),
+                'lastname'  => $person->getLastName(),
+                'email'     => $person->getEmail()
+            )
+        );
+
+        if($format == 'json') {
+            $response = new Response(json_encode($data));
+            $response->headers->set('Content-Type', 'application/json');
+        }
+
+        if($format == 'xml') {
+            $xml = new \SimpleXMLElement('<?xml version="1.0"?><authentication></authentication>');
+            self::arrayToXml($data, $xml);
+            $response = new Response($xml->asXML());
+            $response->headers->set('Content-Type', 'application/xml');
+        }
+
+        return $response;
+    }
+
+    public static function arrayToXml($data, &$xml)
+    {
+        foreach($data as $key => $value) {
+            if(is_array($value)) {
+                if(!is_numeric($key)){
+                    $subnode = $xml->addChild("$key");
+                    self::arrayToXml($value, $subnode);
+                }
+                else{
+                    self::arrayToXml($value, $xml);
+                }
+            }
+            else {
+                $xml->addChild("$key","$value");
+            }
+        }
     }
 }
 
