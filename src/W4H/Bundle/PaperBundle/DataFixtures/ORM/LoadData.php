3c94a6a99b19f2cae33103596478e3d73129f6e7
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-03-22 18:12:50 +0100

    Modify the paper loadData to append new paper only

--- src/W4H/Bundle/PaperBundle/DataFixtures/ORM/LoadData.php
+++ src/W4H/Bundle/PaperBundle/DataFixtures/ORM/LoadData.php
@@ -17,24 +17,38 @@ class LoadData implements FixtureInterface
         $rows = array();
         if (($handle = fopen("src/W4H/Bundle/PaperBundle/DataFixtures/Data/paper.csv", "r")) !== FALSE) {
           $count = 0;
+          $paper_exist = false;
+
           while (($data = fgetcsv($handle, 5000, ",", '"')) !== FALSE) {
             list($session, $paper_number, $title, $subtitle, $number_of_page, $abstract, $author_first_name, $author_middle_name, $author_last_name, $author_mail, $affiliations, $affiliation_location) = $data;
 
             $author = sprintf('%s %s', $author_first_name, $author_last_name);
 
-            if(empty($session) && empty($paper_number) && empty($title) && empty($subtitle) && empty($number_of_page) && empty($abstract)) {
-              printf("\tNew author: %s\n", $author);
-              $rows[$count-1]['authors'] .= ', '.$author;
-            } else {
-              printf("New paper: %s\n", $paper_number);
-               printf("\tNew author: %s\n", $author);
-              $rows[$count] = array(
-                'paper_number'  => $paper_number,
-                'title'         => $title,
-                'authors'       => $author,
-              );
-              $count++;
+            if(!empty($paper_number) && $paper = $manager->getRepository('W4HPaperBundle:Paper')->findOneBy(array('paper_number' => $paper_number))) {
+              printf("Paper exist: %s\n", $paper_number);
+              $paper_exist = true;
+            } elseif(!empty($paper_number)) {
+              $paper_exist = false;
             }
+
+            if(!$paper_exist) {
+
+              if(empty($session) && empty($paper_number) && empty($title) && empty($subtitle) && empty($number_of_page) && empty($abstract)) {
+                printf("\tNew author: %s\n", $author);
+                $rows[$count-1]['authors'] .= ', '.$author;
+              } else {
+                printf("New paper: %s\n", $paper_number);
+                printf("\tNew author: %s\n", $author);
+                $rows[$count] = array(
+                  'paper_number'  => $paper_number,
+                  'title'         => $title,
+                  'authors'       => $author,
+                );
+                $count++;
+              }
+
+            }
+
           }
 
           fclose($handle);
@@ -43,11 +57,11 @@ class LoadData implements FixtureInterface
         $papers = array();
         foreach($rows as $k => $row)
         {
-          $papers[$k] = new Paper();
-          $papers[$k]->setPaperNumber($row['paper_number']);
-          $papers[$k]->setTitle($row['title']);
-          $papers[$k]->setAuthors($row['authors']);
-          $manager->persist($papers[$k]);
+            $papers[$k] = new Paper();
+            $papers[$k]->setPaperNumber($row['paper_number']);
+            $papers[$k]->setTitle($row['title']);
+            $papers[$k]->setAuthors($row['authors']);
+            $manager->persist($papers[$k]);
         }
 
         $manager->flush();
